c1916339cab2444cd2d326f0bd3cb395028a05d9
KshitijNegi12 <23kshitij.negi@gmail.com>
2025-06-09 14:32:35 +0530

    exit builtin #1

--- src/main.cs
+++ src/main.cs
@@ -4,5 +4,10 @@ using System.Net.Sockets;
 while(true){
     Console.Write("$ ");
     var command = Console.ReadLine();
-    Console.WriteLine($"{command}: command not found");
+    if (string.IsNullOrWhiteSpace(command)) break;
+
+    var parts = command.Split(' ');
+    if (parts.Length > 0 && parts[0] == "exit") break;
+
+    Console.WriteLine($"{parts[0]}: command not found");
 }
